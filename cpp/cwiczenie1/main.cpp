e77446b3441ac55ef7fe956b68e04a4b0f2423b8
1Wolfik <daniil.vovk@e-zstih.edu.pl>
2024-03-19 13:59:42 +0000

    studentmachine send

---
+++ cpp/cwiczenie1/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int[4]={6,3,2,8}
+	for (int x=0; x<4; x++){
+	for(int x2=0; x2<4: x2 ++){
+	if(t[x]>t[x2]){
+
+
+
+
+}
+}
+}
+cout<<"maksymalna "<<maks;
+
+	return 0;
+}
